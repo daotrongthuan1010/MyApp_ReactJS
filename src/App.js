547f3361c8f35ad6a9d2f715a18bcc178fc0e1a4
daotrongthuan1010 <conxinloime1010@gmail.com>
2023-02-27 22:40:49 +0700

    study react js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,71 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class Header extends React.Component {
+  render() {
+    return <p>{this.props.title}</p>;
+  }
 }
+
+class Action extends React.Component{
+
+  render(){
+    return (
+    <div>
+      <button>What should I do?</button>
+    </div>
+    )
+  }
+}
+class AddOption extends React.Component{
+  render(){
+    return (
+      <div>
+        <input type={'datetime-local'}></input>
+      </div>
+    )
+  }
+}
+class Options extends React.Component{
+  render(){
+    return(
+      <select defaultValue={props.defaultValue}>
+      {props.options.map((option, index) => (
+        <Option key={index} value={option.value} label={option.label} />
+      ))}
+    </select>
+    )
+  }
+}
+class Option extends React.Component{
+  render(){
+    return <option value={props.value}>{props.key}</option>;
+  }
+}
+
+class App extends React.Component {
+  render() {
+    const options = [
+      { value: 'option1', label: 'Option 1' },
+      { value: 'option2', label: 'Option 2' },
+      { value: 'option3', label: 'Option 3' },
+    ];
+    return (
+      <div>
+        <h1>Title</h1>
+        <Action/>
+        <Header title = "No hope interview !!" />
+        <Header />
+        <AddOption></AddOption>
+        <AddOption></AddOption>
+        <Options options={options} defaultValue={options[0].value}/>
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
+
 
 export default App;
